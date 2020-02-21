84b9ff578f432b3da4e834aa80dee78644f2e120
Jeremiah Buddenhagen <git@bitspill.net>
2020-02-21 15:25:48 -0800

    update node/deps & standard & move to @oipwg

--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
-import { Insight } from 'insight-explorer'
+import { Insight } from '@oipwg/insight-explorer'
 
 import URLExplorer from './URLExplorer.js'
 
-const default_coins = {
-	bitcoin: {
-		usd: {
-			insight: "https://insight.bitpay.com/api",
-			transform: function(response){
-				if (response && response.data && response.data.bitstamp){
-					return response.data.bitstamp
-				}
-
-				return
-			}
-		}
-	},
-	litecoin: {
-		usd: {
-			url: "https://api.coinmarketcap.com/v1/ticker/litecoin/",
-			transform: function(response){
-				if (response && response[0] && response[0].price_usd){
-					return parseFloat(response[0].price_usd)
-				}
-
-				return
-			}
-		}
-	},
-	flo: {
-		usd: {
-			insight: "https://livenet.flocha.in/api",
-			transform: function(response){
-				if (response && response.data && response.data.bitstamp){
-					return response.data.bitstamp
-				}
-
-				return
-			}
-		}
-	}
+const defaultCoins = {
+  bitcoin: {
+    usd: {
+      insight: 'https://insight.bitpay.com/api',
+      transform: function (response) {
+        if (response && response.data && response.data.bitstamp) {
+          return response.data.bitstamp
+        }
+      }
+    }
+  },
+  litecoin: {
+    usd: {
+      url: 'https://api.coinmarketcap.com/v1/ticker/litecoin/',
+      transform: function (response) {
+        if (response && response[0] && response[0].price_usd) {
+          return parseFloat(response[0].price_usd)
+        }
+      }
+    }
+  },
+  flo: {
+    usd: {
+      insight: 'https://livenet.flocha.in/api',
+      transform: function (response) {
+        if (response && response.data && response.data.bitstamp) {
+          return response.data.bitstamp
+        }
+      }
+    }
+  },
+  raven: {
+    usd: {
+      url: 'https://api.coinmarketcap.com/v1/ticker/ravencoin/',
+      transform: function (response) {
+        if (response && response[0] && response[0].price_usd) {
+          return parseFloat(response[0].price_usd)
+        }
+      }
+    }
+  }
 }
 
 /**
@@ -47,17 +51,17 @@ const default_coins = {
  * @property {FiatObject} CoinName.Fiat - Information about the Fiat to convert for the specific coin
  * @example
  * {
- * 	'bitcoin': {
- * 		'usd': {
- * 			'url': "https://api.coinmarketcap.com/v2/ticker/1/",
- * 			'transform': function(data){
- * 				if (data && data.data && data.data.quotes && data.data.quotes.USD && data.data.quotes.USD.price)
- * 					return data.data.quotes.USD.price
+ *   'bitcoin': {
+ *     'usd': {
+ *       'url': "https://api.coinmarketcap.com/v2/ticker/1/",
+ *       'transform': function(data){
+ *         if (data && data.data && data.data.quotes && data.data.quotes.USD && data.data.quotes.USD.price)
+ *           return data.data.quotes.USD.price
  *
- * 				return
- * 			}
- * 		}
- * 	}
+ *         return
+ *       }
+ *     }
+ *   }
  * }
  */
 
@@ -69,99 +73,102 @@ const default_coins = {
  * @property {function} [FiatType.transform] - A function that transforms the data response from the URL/Insight server to return the float conversion value
  * @example
  * {
- * 	'usd': {
- * 		'url': "https://api.coinmarketcap.com/v2/ticker/1/",
- * 		'transform': function(data){
- * 			if (data && data.data && data.data.quotes && data.data.quotes.USD && data.data.quotes.USD.price)
- * 				return data.data.quotes.USD.price
+ *   'usd': {
+ *     'url': "https://api.coinmarketcap.com/v2/ticker/1/",
+ *     'transform': function(data){
+ *       if (data && data.data && data.data.quotes && data.data.quotes.USD && data.data.quotes.USD.price)
+ *         return data.data.quotes.USD.price
  *
- * 			return
- * 		}
- * 	}
+ *       return
+ *     }
+ *   }
  * }
  */
 
-const DEFAULT_TRANSFORM_FUNCTION = function(response){ return response }
+const DEFAULT_TRANSFORM_FUNCTION = function (response) { return response }
 
 const aliases = {
-	bitcoin: "btc",
-	litecoin: "ltc"
+  bitcoin: 'btc',
+  litecoin: 'ltc',
+  raven: 'rvn'
 }
 
 /** Calculate Exchange rates for Cryptocurrencies */
 class Exchange {
-	/**
-	 * Create a new Exchange Rate checker
-	 * ##### Example
-	 * ```
-	 * import Exchange from 'oip-exchange-rate';
-	 * 
-	 * var exchange = new Exchange();
-	 * ```
-	 * @param  {CoinObject} coins - Object containing CoinsObjects
-	 */
-	constructor(coins){
-		if (coins && typeof coins === "object") {
-			// If we are defined, attempt to merge
-			this.coins = Object.assign({}, default_coins, coins);
-		} else {
-			this.coins = default_coins
-		}
+  /**
+   * Create a new Exchange Rate checker
+   * ##### Example
+   * ```
+   * import Exchange from 'oip-exchange-rate';
+   *
+   * let exchange = new Exchange();
+   * ```
+   * @param  {CoinObject} [coins] - Object containing CoinsObjects
+   */
+  constructor (coins) {
+    if (coins && typeof coins === 'object') {
+      // If we are defined, attempt to merge
+      this.coins = Object.assign({}, defaultCoins, coins)
+    } else {
+      this.coins = defaultCoins
+    }
 
-		this.explorers = {}
-	}
-	/**
-	 * @param  {string} coin - The string coin name (ex. "bitcoin")
-	 * @param  {string} fiat - The string fiat (ex. "usd")
-	 * @return {Promise<number>} Returns a Promise that will resolve to the number value of the exchange rate
-	 * @example <caption>Get the Exchange rate for USD per Bitcoin</caption>
-	 * import Exchange from 'oip-exchange-rate';
-	 *
-	 * var exchange = new Exchange();
-	 *
-	 * exchange.getExchangeRate("bitcoin", "usd").then((rate) => {
-	 * 	console.log(rate);
-	 * })
-	 */
-	async getExchangeRate(coin, fiat){
-		var matchedTransform = DEFAULT_TRANSFORM_FUNCTION;
+    this.explorers = {}
+  }
 
-		if (coin === fiat){
-			return 1
-		}
+  /**
+   * @param  {string} coin - The string coin name (ex. "bitcoin")
+   * @param  {string} fiat - The string fiat (ex. "usd")
+   * @return {Promise<number>} Returns a Promise that will resolve to the number value of the exchange rate
+   * @example <caption>Get the Exchange rate for USD per Bitcoin</caption>
+   * import Exchange from 'oip-exchange-rate';
+   *
+   * let exchange = new Exchange();
+   *
+   * exchange.getExchangeRate("bitcoin", "usd").then((rate) => {
+   *   console.log(rate);
+   * })
+   */
+  async getExchangeRate (coin, fiat) {
+    let matchedTransform = DEFAULT_TRANSFORM_FUNCTION
 
-		for (var c in this.coins){
-			if (c === coin || coin === aliases[c]){
-				for (var fiat_type in this.coins[c]){
-					if (fiat_type === fiat){
-						if (!this.explorers[c])
-							this.explorers[c] = {}
+    if (coin === fiat) {
+      return 1
+    }
 
-						if (!this.explorers[c][fiat_type]){
-							if (this.coins[c][fiat_type].insight){
-								this.explorers[c][fiat_type] = new Insight(this.coins[c][fiat_type].insight)
-							} else if (this.coins[c][fiat_type].url) {
-								this.explorers[c][fiat_type] = new URLExplorer(this.coins[c][fiat_type].url, this.coins[c][fiat_type].transform)
-							}
-						}
+    for (const c in this.coins) {
+      if (!Object.prototype.hasOwnProperty.call(this.coins, c)) continue
+      if (c === coin || coin === aliases[c]) {
+        for (const fiatType in this.coins[c]) {
+          if (!Object.prototype.hasOwnProperty.call(this.coins[c], fiatType)) continue
+          if (fiatType === fiat) {
+            if (!this.explorers[c]) { this.explorers[c] = {} }
 
-						if (this.explorers[c][fiat_type]){
-							matchedTransform = this.coins[c][fiat_type].transform;
-							
-							let info = await this.explorers[c][fiat_type].getExchangeRate()
-							let rate = info
+            if (!this.explorers[c][fiatType]) {
+              if (this.coins[c][fiatType].insight) {
+                this.explorers[c][fiatType] = new Insight(this.coins[c][fiatType].insight)
+              } else if (this.coins[c][fiatType].url) {
+                this.explorers[c][fiatType] = new URLExplorer(this.coins[c][fiatType].url, this.coins[c][fiatType].transform)
+              }
+            }
 
-							if (this.coins[c][fiat_type].transform){
-								rate = matchedTransform(info);
-							}
+            if (this.explorers[c][fiatType]) {
+              matchedTransform = this.coins[c][fiatType].transform
 
-							return rate
-						}
-					}
-				}
-			}
-		}
-	}
+              const info = await this.explorers[c][fiatType].getExchangeRate()
+              let rate = info
+
+              if (this.coins[c][fiatType].transform) {
+                rate = matchedTransform(info)
+              }
+
+              return rate
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 module.exports = Exchange
